2a0034219673ba64ef4a110dffb5b636aac8948c
nakano16180 <nakano16180@gmail.com>
2019-04-12 17:09:22 +0900

    add function (select_topic)

--- scripts/msg_log.py
+++ scripts/msg_log.py
@@ -14,8 +14,11 @@ def get_topic_and_type():
     for i in range(0,len(bag.get_type_and_topic_info()[1].values())):
         types.append(bag.get_type_and_topic_info()[1].values()[i][0])
         
+    count = 0
     for key, val in zip(topics, types):
-        print key, val
+        print count, key, val
+        count += 1
+    return topics
 
 def msg2json(msg, indent):
     try:
@@ -28,18 +31,33 @@ def msg2json(msg, indent):
         #print indent*"    ", msg
         return msg
 
-#for topic, msg, t in bag.read_messages():
-#    bagdata = {
-#        "topic": topic,
-#        "timestamp": str(t),
-#        "data": msg2json(msg, 0)
-#    }
-    #print "topic: ", topic
-    #print "msg: \n", msg
-    #print "time: ", str(t)
-    #print "\n-----------------------------"
+def bag2json(topic_list):
+    for topic, msg, t in bag.read_messages(topics=topic_list):
+    #    bagdata = {
+    #        "topic": topic,
+    #        "timestamp": str(t),
+    #        "data": msg2json(msg, 0)
+    #    }
+        print "topic: ", topic
+        #print "msg: \n", msg
+        #print "time: ", str(t)
+        #print "\n-----------------------------"
 
-get_topic_and_type()
+def select():
+    l = [int(i) for i in raw_input().split()]  #error
+    return l
+
+def select_topic():
+    topic_list = get_topic_and_type()
+
+    index_list = select()
+    tl = []
+    for i in index_list:
+        print topic_list[i]
+        tl.append(topic_list[i])
+    bag2json(tl)
+
+select_topic()
 bag.close()
 
 
